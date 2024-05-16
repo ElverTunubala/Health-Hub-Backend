9855f1c6d216dc2df82c5380f482bc018a851c40
Daniel Alejandro <dalona126@hotmail.com>
2024-05-15 21:35:13 -0500

    Create entity roles

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,18 @@
+import { RolesModule } from './roles/roles.module';
+
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
+    RolesModule,
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: 'localhost',
       port: 3306,
       username: 'root',
-      password: 'riwi123',
+      password: 'riwi',
       database: 'health_database',
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
@@ -17,4 +20,4 @@ import { TypeOrmModule } from '@nestjs/typeorm';
     UserModule,
   ],
 })
-export class AppModule {}
+export class AppModule { }
