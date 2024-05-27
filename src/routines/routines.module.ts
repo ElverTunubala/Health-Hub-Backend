9f5b8481a1d4ad1d2bd089c59e29f7a360f7f5f3
ELverTunubala <elvertunubala@gmail.com>
2024-05-27 14:57:52 -0500

    add RoutinesController

--- src/routines/routines.module.ts
+++ src/routines/routines.module.ts
@@ -5,11 +5,12 @@ import {UserEntity} from '../user/user.entity';
 import {RoutineTypesEntity} from '../routine_types/routine_types.entity';
 import {UserService} from '../user/user.service';
 import {RoutinesService} from './routines.service';
+import {RoutinesController} from './routines.controller';
 
 
 @Module({
     imports: [TypeOrmModule.forFeature([RoutinesEntity, UserEntity, RoutineTypesEntity])],
-    controllers: [],
+    controllers: [RoutinesController],
     providers: [RoutinesService, UserService],
 })
 export class RoutinesModule {}
